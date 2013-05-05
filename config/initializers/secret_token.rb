1a4a4cc7407a92a7a90b0783de69b209b72c7cb9
Gremglee <gremglee@gmail.com>
2013-05-05 04:56:00 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mindroad::Application.config.secret_token = 'dd9b45778895f1f10c48f7371b63f65237dbc1652eaab45ddc681e013dc1761a93cde95294f47a58123a50bbf9b492fcb1ea3d047c81e2f1ab93297f1ea80c22'
